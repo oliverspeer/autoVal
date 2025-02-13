af8b8c94d30f753d0b1aa612ffad4415f9dfc7ef
oliverspeer <oliver.speer@startmail.com>
2025-02-13 21:36:46 +0100

    organize qc data

--- transferQCdata.R
+++ transferQCdata.R
@@ -1,7 +1,8 @@
-setwd("/home/olli/R_local/autoVal")
+setwd("C:/R_local/autoVal")
 
 source("StartUp.R")
 StartUpRoutine()
+
 
 # Load the data
 # function to load QC data from different file formats
@@ -22,6 +23,7 @@ fun.load.qc.file <- function(file_path, remove_cols, delim = NULL, clean_level =
 }
 
 # load QC data
+setwd("C:/R_local/autoVal")
 dxi_1_immunoassay_qc <- fun.load.qc.file("DXI 1 Immunoassay QC.xlsx", 9:11)
 dxi_1_stfr_qc <- fun.load.qc.file("DXI 1 sTfR QC.xlsx", 9:11)
 dxi_1_speciality_qc <- fun.load.qc.file("DXI 1 Speciality QC.xlsx", 9:11)
@@ -30,6 +32,13 @@ dxi_2_amh_qc <- fun.load.qc.file("DXI 2 AMH QC.xlsx", 9:11)
 dxi1_cardio_1_0 <- fun.load.qc.file("DXI1 Cardio 1.0.csv", 9:12, delim = ";", clean_level = TRUE)
 dxi1_tumormarker_1_0 <- fun.load.qc.file("DXI1 Tumormarker 1.0.csv", 9:12, delim = ";", clean_level = TRUE)
 
+dxi_1_immunoassay_qc25 <- fun.load.qc.file("DXI 1 Immunoassay QC25.xlsx", 6)
+dxi_1_stfr_qc25 <- fun.load.qc.file("DXI 1 sTfR QC25.xlsx", 6)
+dxi_1_speciality_qc25 <- fun.load.qc.file("DXI 1 Speciality QC25.xlsx", 6)
+dxi_1_shbg_qc25 <- fun.load.qc.file("DXI 1 SHBG QC25.xlsx", 6)
+dxi_2_amh_qc25 <- fun.load.qc.file("DXI 1 AMH QC25.xlsx", 6)
+dxi1_cardio_1_025 <- fun.load.qc.file("DXI1 Cardio 25.csv", 6, delim = ";", clean_level = TRUE)
+dxi1_tumormarker_1_025 <- fun.load.qc.file("DXI1 Tumormarker QC25.xlsx", 6)
 # bind all data frames together
 qc.dat <- bind_rows(
   dxi_1_immunoassay_qc, 
@@ -38,8 +47,57 @@ qc.dat <- bind_rows(
   dxi_1_shbg_qc, 
   dxi_2_amh_qc, 
   dxi1_cardio_1_0, 
-  dxi1_tumormarker_1_0
-  )
+  dxi1_tumormarker_1_0) |> 
+  
+  rename(LotNr = "Lot No.") |>
+  select(Parameter, Level, Date, Zielwert, SD, LotNr)
+  
+
+  
+  # group_by(Parameter, Level, Zielwert) |> 
+  # reframe(
+  #   Date = Date,
+  #   Zielwert = first(Zielwert),
+  #   Level = first(Level),
+  #   SD = first(SD),
+  #   LotNr = first(LotNr)
+  # ) |> 
+  # ungroup()
+
+qc.dat1 <- bind_rows(
+  dxi_1_immunoassay_qc25, 
+  dxi_1_stfr_qc25, 
+  dxi_1_speciality_qc25, 
+  dxi_1_shbg_qc25, 
+  dxi_2_amh_qc25, 
+  dxi1_cardio_1_025, 
+  dxi1_tumormarker_1_025) 
+
+# |> 
+#   
+#   group_by(Parameter, Level, Zielwert) |> 
+#   reframe(
+#     Date = Date,
+#     Zielwert = first(Zielwert),
+#     Level = first(Level),
+#     SD = first(SD),
+#     LotNr = first(LotNr)
+#   ) |> 
+#   ungroup()
+
+
+qc.dat <- bind_rows(qc.dat, qc.dat1) |> 
+  
+  group_by(Parameter, Level, Zielwert) |> 
+  summarise(
+    minDate = min(Date, na.rm = TRUE),
+    maxDate = max(Date, na.rm = TRUE),
+    Zielwert = first(Zielwert),
+    Level = first(Level),
+    SD = first(SD),
+    LotNr = first(LotNr)
+  ) |> 
+  ungroup()
 
 translate.df <- dbReadTable(con, "TranslationData")
 write.xlsx(translate.df, "TranslationData.xlsx")
@@ -66,7 +124,7 @@ map.data.types <- function(df) {
 col.types <- map.data.types(qc.dat)
 
 # construct a CREATE TABLE statement for val.dat using the mapped data types
-create.table.statement <- paste0("CREATE TABLE IF NOT EXISTS DxIvalData (",
+create.table.statement <- paste0("CREATE TABLE IF NOT EXISTS QCData (",
                                  paste(
                                    sapply(names(col.types), function(name) paste0('"', name, '"')),
                                    col.types, 
