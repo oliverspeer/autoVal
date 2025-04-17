ec8fd35f825d0f068288a04cfc242e09f1aac327
oliverspeer <oliver.speer@startmail.com>
2025-04-17 11:00:11 +0200

    results with < or > will be NA DoseResults_c Time conversion

--- R/fun_load_process_DxI9000_data.R
+++ R/fun_load_process_DxI9000_data.R
@@ -1,15 +1,15 @@
 fun_load_process_DxI9000_data <- function(csv_path, sum_dat) {
-  source("StartUp.R")
- StartUpRoutine()
- source("R/fun_convert_to_standard.R")
-  library(targets)
- tar_load(molmass_data)  
- tar_load(raw_data_csv)
- 
- sum.dat <- molmass_data  
- csv_path <- raw_data_csv
- 
- # Lese die CSV-Datei ein und benenne die Spalten um
+  #  source("StartUp.R")
+  # StartUpRoutine()
+  # source("R/fun_convert_to_standard.R")
+  #  library(targets)
+  # tar_load(molmass_data)
+  # tar_load(raw_data_csv)
+  #
+  # sum_dat <- molmass_data
+  # csv_path <- raw_data_csv
+  
+  # Lese die CSV-Datei ein und benenne die Spalten um
   val.dat <- read_csv(csv_path) |>
     rename(
       PatientID = `Patient ID`,
@@ -41,14 +41,14 @@ fun_load_process_DxI9000_data <- function(csv_path, sum_dat) {
   
   merged.dat <- df |>
     
-    merge(sum.dat, by = "TestOrderCode", all.x = TRUE) |>
+    merge(sum_dat, by = "TestOrderCode", all.x = TRUE) |>
     
     mutate(
       Symbol = sub("([><]).*", "\\1", DoseResult),
       
       Symbol = ifelse(Symbol == DoseResult, NA, Symbol),
       
-      DoseResult = sub("[><]", "", DoseResult) |> as.numeric()
+      DoseResult = sub("[><]", NA, DoseResult) |> as.numeric()
       
     )
   
@@ -56,106 +56,7 @@ fun_load_process_DxI9000_data <- function(csv_path, sum_dat) {
   
   # Function to convert units to a standard form----------------------------
   
-  convert_to_standard <- function(unit) {
-    install_unit("IU", "umol/min", "international unit")
-    
-    # Define a dictionary for common unit conversions
-    
-    unit_dict <- list(
-      "ug/l" = set_units(1, "ug/l"),
-      
-      "ng/ml" = set_units(1, "ng/ml"),
-      
-      "nmol/l" = set_units(1, "nmol/l"),
-      
-      "umol/l" = set_units(1, "umol/l"),
-      
-      "pmol/l" = set_units(1, "pmol/l"),
-      
-      "ng/l" = set_units(1, "ng/l"),
-      
-      "pg/ml" = set_units(1, "pg/ml"),
-      
-      "ug/dl" = set_units(1, "ug/dl"),
-      
-      "ng/dl" = set_units(1, "ng/dl"),
-      
-      "mU/l" = set_units(1, "mIU/l"),
-      
-      "mIU/ml" = set_units(1, "mIU/ml"),
-      
-      "uIU/ml" = set_units(1, "uIU/ml"),
-      
-      "U/l" = set_units(1, "IU/l"),
-      
-      "nmol/l" = set_units(1, "nmol/l"),
-      
-      "µg/l" = set_units(1, ug / l),
-      
-      "ng/mL" = set_units(1, ng / ml),
-      
-      "ng/ml" = set_units(1, ng / ml),
-      
-      "nmol/l" = set_units(1, nmol / l),
-      
-      "µmol/l" = set_units(1, umol / l),
-      
-      "pmol/l" = set_units(1, pmol / l),
-      
-      "pmol/L" = set_units(1, pmol / l),
-      
-      "ng/l" = set_units(1, ng / l),
-      
-      "pg/mL" = set_units(1, pg / ml),
-      
-      "µg/dL" = set_units(1, ug / dl),
-      
-      "µg/l" = set_units(1, ug / l),
-      
-      "ng/dL" = set_units(1, ng / dl),
-      
-      "mU/l" = set_units(1, mIU / l),
-      
-      "mlU/l" = set_units(1, mIU / l),
-      # Assumed conversion for demonstration
-      
-      "U/l" = set_units(1, IU / l),
-      
-      "µIU/mL" = set_units(1, uIU / ml),
-      
-      "mIU/mL" = set_units(1, mIU / ml),
-      
-      "nmol/L" = set_units(1, nmol / l),
-      # assuming case insensitivity
-      
-      "nmol/l" = set_units(1, nmol / l),
-      
-      "g/mol" = set_units(1, g / mol)# assuming case insensitivity
-      
-    )
-    
-    
-    
-    
-    
-    
-    
-    # Check if unit exists in dictionary
-    
-    if (!unit #_lower
-        
-        %in% names(unit_dict)) {
-      message(paste("Unit", unit, "not found in dictionary."))
-      
-      return(NULL)
-      
-    }
-    
-    
-    
-    return(unit_dict[[unit]])
-    
-  }
+  
   
   
   
@@ -175,9 +76,9 @@ fun_load_process_DxI9000_data <- function(csv_path, sum_dat) {
       
     }
     
-    from_unit <- convert_to_standard(merged.dat$DoseUnit[i])
+    from_unit <- fun_convert_to_standard(merged.dat$DoseUnit[i])
     
-    to_unit <- convert_to_standard(merged.dat$Einheit_800[i])
+    to_unit <- fun_convert_to_standard(merged.dat$Einheit_800[i])
     
     # Compare the units
     
